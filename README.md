57cf78bc3fb95449e3b42e96d08cdeff8999800f
MarkSian <merksianipar@gmail.com>
2025-01-10 13:54:15 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Ani-Filter
+ Feeling a particular way? Click on the mood that fits your vibe and get some direction on what to watch
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+export default function NavBar() {
+    return (
+        <>
+            <div class="navbar bg-base-300 mx-auto ">
+                <div class="navbar-start">
+                    <button class="btn btn-ghost btn-circle">
+                    ⛩️
+                    </button>
+                </div>
+                <div class="navbar-center">
+                    <a class="btn btn-ghost text-xl">Ani-Filter</a>
+                </div>
+                <div class="navbar-end">
+                    <button class="btn btn-ghost btn-circle">
+                    <svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6 3L6 6H12L12 10H6L6 13L5 13L0 8L5 3L6 3Z" fill="#ffffff"></path> <path d="M16 2L16 14H14L14 2L16 2Z" fill="#ffffff"></path> </g></svg>
+                    </button>
+                </div>
+            </div>
+        
+        </>
+    )
+}
