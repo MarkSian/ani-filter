201fc11df0a6c7962ab9c5519a701a4b01e3d1ee
MarkSian <merksianipar@gmail.com>
2025-01-10 16:37:20 -0700

    created navbar
    SVG's and emoji's added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,20 @@
 export default function NavBar() {
     return (
         <>
-            <div class="navbar bg-base-300 mx-auto ">
+            <div class="navbar bg-base-300 center-container mt-4 py-2 my-2 rounded-[15px]">
                 <div class="navbar-start">
-                    <button class="btn btn-ghost btn-circle">
-                    ⛩️
-                    </button>
+                    <button class="btn btn-ghost btn-circle text-2xl">
+                    ⛩️ 
+                </button>
                 </div>
                 <div class="navbar-center">
                     <a class="btn btn-ghost text-xl">Ani-Filter</a>
                 </div>
                 <div class="navbar-end">
                     <button class="btn btn-ghost btn-circle">
-                    <svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6 3L6 6H12L12 10H6L6 13L5 13L0 8L5 3L6 3Z" fill="#ffffff"></path> <path d="M16 2L16 14H14L14 2L16 2Z" fill="#ffffff"></path> </g></svg>
+<                   svg version="1.1" height="25px" id="Uploaded to svgrepo.com" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" xml:space="preserve" fill="#000000"><g id="SVGRepo_bgCarrier" 
+                    stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier">  
+                    <path class="sharpcorners_een" fill="#ffffff" d="M29.387,12.723l-1.109,1.664L16,6.202L3.723,14.387l-1.109-1.664L6,10.465V5h4v2.798l6-4 L29.387,12.723z M6,14.07V29h7v-8h6v8h7V14.07L16,7.404L6,14.07z"></path> </g></svg>
                     </button>
                 </div>
             </div>
